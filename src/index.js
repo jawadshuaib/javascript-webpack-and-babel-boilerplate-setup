15efeb2bf3f389a9ba1221d9bb7d256a7eeff9ac
Jawad <jawad.php@gmail.com>
2020-12-18 23:14:13 -0500

    Added ESLint Support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-
-// note that this is loaded using the webpack module for css loaders configured in the webpack.conf file
+// note that this is loaded using the webpack module for css loaders configured in
+// the webpack.conf file
 import './sample.css';
 
-const body = document.querySelector ('body');
+const body = document.querySelector('body');
 
 body.textContent = 'Hello World! :)';
